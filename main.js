9b8798a07311348715862e50679fe155f9a9ab3a
sunbooh <148074723+sunbooh@users.noreply.github.com>
2024-05-06 11:49:16 -0300

    aaaa

--- main.js
+++ main.js
@@ -48,12 +48,11 @@ function atualizaCronometro(){
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 
     for (let i=0; i<contadores.length;i++){
-       // contadores[i].textContent = calculaTempo(tempos[i]);   
- // contadores[i].textContent = calculaTempo(tempos[i]); apagar essa linha
-        //document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
-        //document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
-        //document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        //document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+       contadores[i].textContent = calculaTempo(tempos[i]);   
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 
